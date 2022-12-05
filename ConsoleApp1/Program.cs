0e0ee49d6bd5df6f281c6d67b63733ec428b53c7
Ziad Mahmoud <Ziad.Mahmoud21.07@Gmail.com>
2022-12-05 21:21:24 +0200

    Console (Write,Read)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Enter 1st Number: ");
+double Number2 =double.Parse(Console.ReadLine());
+Console.Write("Enter 2nd Number: ");
+double Number = double.Parse(Console.ReadLine());
+
+double Sum=Number2 + Number;
+double sub=Number2 - Number;
+double Mul =Number2 * Number;
+double Div=Number2 / Number;
+
+Console.WriteLine("Sum= "+Sum);
+Console.WriteLine("Sub= {0}",sub);
+Console.WriteLine($"Mul = {Mul}");
+Console.WriteLine("Div" +Div);
+
