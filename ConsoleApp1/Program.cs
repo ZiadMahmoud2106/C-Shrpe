c9c89a7c2cf3c6fff92feefa2378c6d434aa16e0
Ziad Mahmoud <Ziad.Mahmoud21.07@Gmail.com>
2022-12-05 21:30:01 +0200

    String

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,16 +1,38 @@
-﻿
-Console.Write("Enter 1st Number: ");
-double Number2 =double.Parse(Console.ReadLine());
-Console.Write("Enter 2nd Number: ");
-double Number = double.Parse(Console.ReadLine());
+﻿#region Double
 
-double Sum=Number2 + Number;
-double sub=Number2 - Number;
-double Mul =Number2 * Number;
-double Div=Number2 / Number;
+//Console.Write("Enter 1st Number: ");
+//double Number2 = double.Parse(Console.ReadLine());
+//Console.Write("Enter 2nd Number: ");
+//double Number = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Sum= "+Sum);
-Console.WriteLine("Sub= {0}",sub);
-Console.WriteLine($"Mul = {Mul}");
-Console.WriteLine("Div" +Div);
+//double Sum = Number2 + Number;
+//double sub = Number2 - Number;
+//double Mul = Number2 * Number;
+//double Div = Number2 / Number;
 
+//Console.WriteLine("Sum= " + Sum);
+//Console.WriteLine("Sub= {0}", sub);
+//Console.WriteLine($"Mul = {Mul}");
+//Console.WriteLine("Div" + Div);
+
+#endregion
+
+
+#region String
+          // 0123
+//string dd = "ziad";
+
+//Console.WriteLine(dd);
+
+//char z = 'z';
+//char i = 'i';
+//char a = 'a';
+//char d = 'd';
+//Console.WriteLine(z+""+i + "" + a + "" + d);
+//Console.WriteLine($"{z}{i}{a}{d}");
+//char z = dd[0];
+//Console.WriteLine(z);
+
+
+
+#endregion
