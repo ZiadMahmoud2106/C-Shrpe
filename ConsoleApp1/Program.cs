78bac07c9064aba3217c382d0d9707614dc5c34b
Ziad Mahmoud <Ziad.Mahmoud21.07@Gmail.com>
2022-12-08 19:52:38 +0200

    IF

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,20 +18,103 @@
 #endregion
 
 
+
+
+
+
+
+
+
+
+
+
+#region If Math.Max
+
+//Console.Write("Enter Number 1 : ");
+//int num1 = int.Parse(Console.ReadLine());
+//Console.Write("Enter Number 2 : ");
+//int num2 = int.Parse(Console.ReadLine());
+//Console.Write("Enter Number 3 : ");
+//int num3 = int.Parse(Console.ReadLine());
+//Console.Write("Enter Number 4 : ");
+//int num4 = int.Parse(Console.ReadLine());
+//Console.WriteLine(Math.Max(num1,Math.Max(num2,Math.Max(num3,num4))));
+
+
+
+
+//if (num1 % 3 == 0 && num1 % 7 == 0)
+//	Console.WriteLine(num1);
+
+
+
+
+//int x=num1 > num2 ? num1: num2>num1?num2:num1;
+//Console.WriteLine(x);
+
+
+//Console.WriteLine(Math.Max(num1,num2));
+
+
+//Console.Write("Enter Number 1 : ");
+//int num1 = int.Parse(Console.ReadLine());
+//Console.Write("Enter Number 2 : ");
+//int num2 = int.Parse(Console.ReadLine());
+
+//if (num1 > num2)
+//{
+//    Console.WriteLine(num1);
+//}
+//else if (num1 < num2)
+//{
+//    Console.WriteLine(num2);
+//}
+//else
+//{
+//    Console.WriteLine("==");
+//}
+
+
+
+//if (num1 > num2)
+//{
+//	Console.WriteLine(num1);
+//}
+
+//if (num1 < num2)
+//{
+//	Console.WriteLine(num2);
+//}
+
+//if (num1 == num2)
+//{
+//	Console.WriteLine("==");
+//}
+
+
+
+#endregion
 #region String
-          // 0123
+
+
+//           //0123
 //string dd = "ziad";
 
-//Console.WriteLine(dd);
+//Console.WriteLine(dd+"1");
 
 //char z = 'z';
 //char i = 'i';
 //char a = 'a';
 //char d = 'd';
-//Console.WriteLine(z+""+i + "" + a + "" + d);
-//Console.WriteLine($"{z}{i}{a}{d}");
-//char z = dd[0];
-//Console.WriteLine(z);
+
+
+//Console.WriteLine(z + "" + i + "" + a + "" + d+"2");
+//Console.WriteLine($"{z}{i}{a}{d}3");
+
+
+
+//char z1 = dd[3];
+//Console.WriteLine(z1);
 
 
 
